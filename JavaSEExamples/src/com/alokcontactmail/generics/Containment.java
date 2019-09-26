1cf5eaa67bf4b5f0e67abea5a341eed3486485f2
C5072895 <alokcontactmail@gmail.com>
2019-09-26 21:24:26 +0530

    Added Default and static method to interface

--- JavaSEExamples/src/com/alokcontactmail/generics/Containment.java
+++ JavaSEExamples/src/com/alokcontactmail/generics/Containment.java
@@ -5,4 +5,11 @@ public interface Containment<T> {
 	// specific item is contained within 
 	// an object that implements containment.
 	boolean contains(T o);
+	default void display() {
+		System.out.println("Inside Containment default display method : ");
+	}
+	
+	static void Show() {
+		System.out.println("Inside Containment static Show method : ");
+	}
 }
