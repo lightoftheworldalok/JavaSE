ebe19d71f43483a4a700c7903c5adb56d5aeb5af
C5072895 <alokcontactmail@gmail.com>
2019-10-23 21:17:06 +0530

    obtaining property values

---
+++ JavaSEExamples/src/com/alokcontactmail/javalang/ShowProperties.java
@@ -0,0 +1,13 @@
+package com.alokcontactmail.javalang;
+
+public class ShowProperties {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("java.version"));
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("file.separator"));
+
+	}
+
+}
