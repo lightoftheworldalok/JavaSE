8b056fe7a6b8253983288d3b6953f4ae0823528e
C5072895 <alokcontactmail@gmail.com>
2019-11-01 16:02:05 +0530

    Read user input from text

---
+++ JavaSEExamples/src/com/alokcontactmail/javaUtil/InputUsingSystemIn.java
@@ -0,0 +1,13 @@
+package com.alokcontactmail.javaUtil;
+
+import java.util.Scanner;
+
+public class InputUsingSystemIn {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next());
+
+	}
+
+}
