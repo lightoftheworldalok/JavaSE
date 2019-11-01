cc5fb4f63a4761de2b1ea498942f45b633e8587d
C5072895 <alokcontactmail@gmail.com>
2019-11-01 15:46:39 +0530

    Formatting Date and time

---
+++ JavaSEExamples/src/com/alokcontactmail/javaUtil/TableFormatDemo.java
@@ -0,0 +1,16 @@
+package com.alokcontactmail.javaUtil;
+
+import java.util.Formatter;
+
+public class TableFormatDemo {
+
+	public static void main(String[] args) {
+		Formatter fmt = new Formatter();
+		for (int i = 1; i <= 10; i++) {
+			fmt.format("%4d %4d %4d\n", i,i*i,i*i*i);
+		}
+		System.out.println(fmt);
+		fmt.close();
+	}
+
+}
